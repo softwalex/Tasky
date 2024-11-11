f412456a1b2356e853f78d6b5ffba6c000407955
Alexander Denisenko <sashadenisenko6@gmail.com>
2024-11-11 12:31:48 +0200

    Login & signUp windows on XAML

---
+++ TaskClientPC/TaskClientPC/LogIn.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TaskClientPC.TaskyServiceReference;
+
+namespace TaskClientPC
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void EventLogIn(object sender, RoutedEventArgs e)
+        {
+            UserServiceClient userServiceClient = new UserServiceClient();
+            User user = userServiceClient.UserLogin(EmailTextBox.Text, PasswordBox.Password);
+            if (user != null)
+            {
+                MessageBox.Show("WELCOME");
+            }
+            else
+            {
+                MessageBox.Show("EROR");
+            }
+        }
+    }
+}
