fc9f151dc8704e0363b113b96245f37198691e0b
Alexander Denisenko <sashadenisenko6@gmail.com>
2024-11-21 11:07:58 +0200

    Main UserControlWindow

---
+++ TaskClientPC/TaskClientPC/UserControls/Users_UserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskClientPC.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Users_UserControl.xaml
+    /// </summary>
+    public partial class Users_UserControl : UserControl
+    {
+        public Users_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
